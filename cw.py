f12af994ac856891f9e250bf2a6a894ab00c7592
Austin Ngan <angan20@stuy.edu>
2021-09-22 20:54:14 -0400

    created a method that gives a random name from a selected period

--- cw.py
+++ cw.py
@@ -1,9 +1,18 @@
+#Austin Ngan
+#SoftDev
+#K01 -- Random Name Python Practice
+#2021-09-22
+
+import random
+
 pd1 = ["Joe Smith", "Jim Smith", "Leon Lauren"]
 
 pd2 = ["Alex Baron", "Bean Cris", "Chad Johnson"]
 
-for name in pd1:
-    print(name)
-
-for name in pd2:
-    print(name)
+def getName(period):
+    if period==0:
+        randName=random.choice(pd1)
+        print (randName)
+    else:
+        randName2=random.choice(pd2)
+        print (randName2)
