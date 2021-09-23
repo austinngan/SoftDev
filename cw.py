a0c752ad8fd4eea444d9e2e1d6d4b50a7aeefd34
Austin Ngan <angan20@stuy.edu>
2021-09-22 21:04:08 -0400

    added most names for each period

--- cw.py
+++ cw.py
@@ -5,9 +5,18 @@
 
 import random
 
-pd1 = ["Joe Smith", "Jim Smith", "Leon Lauren"]
+pd1 = ["Owen Yaggy", "Haotian Gan", "Ishraq Mahid","Ivan Lam","Michelle Lo"
+        "Christopher Liu","Ivan Mijacika","Lucas Lee","Josephine Lee","Rayat Roy",
+        "Emma Buller","William Chen","Rachel Xiao","Andrew Juang","Yuqing Wu"
+        "Renggeng Zheng","Annabel Zhang","Alejandro Alonso","Deven Maheshwari",
+        "Oscar Wang","Tami Takada","Yusuf Elsharawy","Ella Krechmer","Tomas Acuna",
+        "Tina Nguyen","Theo Fahey","Sadid Ethun"]
 
-pd2 = ["Alex Baron", "Bean Cris", "Chad Johnson"]
+pd2 = ["Patrick Ging","Wenhao Dong","Raymond Yeung","Kevin Cao","Michael Borczuk",
+        "Alif Abdullah","Justin Zou","Andy Lin","Shadman Rakib","David Chong",
+        "Daniel Sooknanan","Cameron Nelson","Austin Ngan","Yaying Liang Li"
+        ,"Eric Guo","Noakai Aronesty","Roshani Shrestha","Yoonah Chang",
+        "Qina Liu","Han Zhang","Joshua Kloepfer"]
 
 def getName(period):
     if period==0:
