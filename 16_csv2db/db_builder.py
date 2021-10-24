d4192907156ac6eff6efe2712e32b5f27146b4ff
Austin Ngan <angan20@stuy.edu>
2021-10-24 19:21:44 -0400

    finished making code for populating the classes table

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -16,25 +16,31 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 
 
 # < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
-command = "CREATE TABLE roster(name TEXT, age INTEGER, id INTEGER)" 
-#c.execute(command)
+command = "CREATE TABLE roster(name TEXT, age INTEGER, id INTEGER)"
+c.execute(command)
+
+command2 = "CREATE TABLE courses(code TEXT, mark INTEGER, id INTEGER)"
+c.execute(command2)
 
 with open("students.csv", newline='') as csvfile:
+    '''adds name, age, and id to the corresponding parts of the roster table'''
     studentReader = csv.DictReader(csvfile)
-    for row in studentReader:
-    	
-    	#age=row['age']
-    	#identification=row['id']
+    for row in studentReader: #loops through each line in the .csv file
+    #age=row['age']
+    #identification=row['id']
 	#print(name)
 	#print(age)
 	#print(identification)
 	#addData = "INSERT INTO roster VALUES (name, age, identification);"
         c.execute("INSERT INTO roster VALUES("+"'"+row['name']+"'"+", "+row['age']+", "+row['id']+")")
 
+with open("courses.csv", newline='') as csvfile:
+    '''adds code, mark, and id to the corresponding parts of the class table'''
+    courseReader = csv.DictReader(csvfile)
+    for row in courseReader: #loops through each line in the .csv file
+        c.execute("INSERT INTO courses VALUES("+"'"+row['code']+"'"+", "+row['mark']+", "+row['id']+")")
+
 #==========================================================
 
 db.commit() #save changes
 db.close()  #close database
-
-
-
