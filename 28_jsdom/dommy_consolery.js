826acb819d06ebaaf8bebb718876eeaa3c97dd05
Austin Ngan <angan20@stuy.edu>
2022-02-08 09:22:14 -0500

    added 28

---
+++ 28_jsdom/dommy_consolery.js
@@ -0,0 +1,137 @@
+/*
+   your PPTASK:
+   
+   Test drive each bit of code in this file,
+    and insert comments galore, indicating anything
+     you discover,
+    	have questions about,
+    		or otherwise deem notable.
+    		
+    		Write with your future self or teammates in mind.
+    		
+    		If you find yourself falling out of flow mode, consult 
+    		other teams
+    		MDN
+
+   A few comments have been pre-filled for you...
+   
+   (delete this block comment once you are done)
+*/
+
+// Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon 
+// SoftDev pd0
+// K28 -- Getting more comfortable with the dev console and the DOM
+// 2022-02-08t
+// --------------------------------------------------
+
+
+//send diagnostic output to console
+//(Ctrl-Shift-K in Firefox to reveal console)
+console.log("AYO");
+
+var i = "hello";
+var j = 20;
+
+
+//assign an anonymous fxn to a var
+var f = function(x) {
+  var j=30;
+  return j+x;
+};
+
+
+//instantiate an object
+var o = { 'name' : 'Thluffy',
+          age : 15,
+          items : [10, 20, 30, 40],
+          morestuff : {a : 1, b : 'ayo'},
+          func : function(x) {
+            return x+30;
+          }
+        };
+
+
+var addItem = function(text) {
+  var list = document.getElementById("thelist");
+  var newitem = document.createElement("li");
+  newitem.innerHTML = text;
+  list.appendChild(newitem);
+};
+
+
+var removeItem = function(n) {
+  var listitems = document.getElementsByTagName('li');
+  listitems[n].remove();
+};
+
+
+var red = function() {
+  var items = document.getElementsByTagName("li");
+  for(var i = 0; i < items.length; i++) {
+    items[i].classList.add('red');
+  }
+};
+
+
+var stripe = function() {
+  var items = document.getElementsByTagName("li");
+  for(var i = 0; i < items.length; i++) {
+    if (i%2==0){
+      items[i].classList.add('red');
+    } else {
+      items[i].classList.add('blue');
+    }
+  }
+};
+
+//insert your implementations here for...
+var fac = (n) =>{
+	if (n==0){
+		return 1;
+		}
+		
+	else{
+		return (fac(n-1)*n);
+	}
+		
+}
+
+var fib = (n) => {
+	if (n==0){
+		return 0p;
+	}
+// could also do if (n<=1){return n}
+
+	if (n==1){
+		return 1;
+	}
+	else{
+		return (fib(n-1)+fib(n-2));
+	}
+}
+
+
+var gcd = (a,b) =>{
+	if (a>b){
+		if (a%b==0){
+			return b;
+	}
+		else{
+			return (gcd(b, (a%b)));
+		}
+	
+	}
+	if (b>a){
+		if (b%a==0){
+			return a;
+	}
+		else{
+			return (gcd(a, (b%a)));
+		}
+	
+	}
+	else{
+		return a;
+		}
+	}
+
